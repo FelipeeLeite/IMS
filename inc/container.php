baef22a31a4e368f8b990dea82d71e760384eaef
felipe.leite <felipe.leite@fec.local>
2023-09-20 09:05:00 -0300

    My First Commit

---
+++ inc/container.php
@@ -0,0 +1,14 @@
+</head>
+<body class="bg-gradient bg-dark bg-opacity-25 ">
+<nav role="navigation" class="navbar navbar-dark navbar-static-top bg-primary bg-gradient">
+    <div class="container">
+
+      <div class="navbar-header">
+        <a href="" class="navbar-brand">Controle de Estoque</a>
+      </div>
+    </div>
+  </nav>
+	
+	<div class="container">
+  
+	
